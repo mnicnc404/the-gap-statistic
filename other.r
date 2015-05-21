c2b5d3c72aca057a848a233c7cb8fc02d50dfefa
mnicnc404 <mnicnc404@gmail.com>
2015-05-21 15:36:36 +0800

    KL, CH, s, H finished. redundant code removed from gap.r.

--- other.r
+++ other.r
@@ -1,7 +1,5 @@
-
-CH = function(data, max = 10, method = kmeans, ...)
-{	x = data
-	if (is.matrix(x)) x = data.frame(x)
+CH.stat = function(data, max = 10, method = kmeans, ...)
+{	x = data.frame(data)
 	## CH(k=1) is not defined since there's a (k-1) in the denominator; even if we replace (k-1) by k, B(k=1) would be 0. The maximum would never occur at k=1. ##
 	w = b = ch = rep(0, max)
 	for (k in 2:max) 
@@ -10,5 +8,37 @@ CH = function(data, max = 10, method = kmeans, ...)
 		b[k] = res$betweenss
 		ch[k] = (b[k]/(k-1))/(w[k]/(nrow(x)-k))
 	}
-	return (list(Tabs = cbind(w.k = w, b.k = b, ch.k = ch), GlobalMaxClusterNum = which.max(ch)))
+	return (list(Tabs = cbind(w.k = w, b.k = b, ch.k = ch), ClusterNum = which.max(ch)))
+}
+
+KL.stat = function(data, max = 10, method = kmeans, ...)
+{	x = data.frame(data)
+	w = diff = rep(0, max+1)
+	kl = rep(0, max)
+	for (k in 1:(max+1)) w[k] = method(x, k, ...)$tot.withinss
+	for (k in 2:(max+1)) diff[k] = w[k-1]*(k-1)^(2/length(x)) - w[k]*k^(2/length(x))
+	for (k in 2:max) kl[k] = abs(diff[k]/diff[k+1])
+	return (list(kl = kl, ClusterNum = which.max(kl)))
+}
+
+H.stat = function(data, max = 10, method = kmeans, ...)
+{	x = data.frame(data)
+	w = rep(0, max+1)
+	h = rep(0, max)
+	for (k in 1:(max+1)) w[k] = method(x, k, ...)$tot.withinss
+	clus = 0
+	for (k in 1:max) 
+	{	h[k] = (w[k]/w[k+1] - 1)/(nrow(x) - k - 1)
+		if (h[k] <= 10 & clus == 0) clus = k
+	}
+	if (clus == 0) clus = which.min(h)
+	return (list(Hartigan = h, ClusterNum = clus))
+}
+
+s.stat = function(data, max = 10, method = kmeans, ...)
+{	require(cluster)
+	x = data.frame(data)
+	s = rep(0, max)
+	for (k in 2:max) s[k] = mean(silhouette(method(x, k ,...)$cluster, dist(x))[,3])
+	return (list(AvgSilhouette = s, ClusterNum = which.max(s)))
 }
