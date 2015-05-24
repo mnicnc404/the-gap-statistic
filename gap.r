114c82f4d24798c4b507f2a1d2a3f509cd2d2332
mnicnc404 <mnicnc404@gmail.com>
2015-05-24 18:42:24 +0800

    hclust done.

--- gap.r
+++ gap.r
@@ -1,6 +1,5 @@
-ln.wk = function(x, kb = 1, method, ...)
-{	label = method(x, kb, ...)$cluster
-	return (log(sum(sapply(split(1:nrow(x), label), function(ki) {
+ln.wk = function(x, label)
+{	return (log(sum(sapply(split(1:nrow(x), label), function(ki) {
 				pair = x[ki, ,drop = F]
 				sum(dist(pair))/(2*nrow(pair))
 			}))))
@@ -28,13 +27,19 @@ ref.dist = function(x, pc = T)
 
 gap.stat = function(data, max = 10, method = kmeans, pc = T, B = 50, tibs = T, ...)
 {	x = data
+	is.hclust = as.character(substitute(method)) == "hclust"
 	if (is.matrix(x)) x = data.frame(x) 
 	ln.w = e.ln.w = s = oriGap = rep(0, max)
 	e.ln.ws = matrix(0, B, max)
-	for (k in 1:max) ln.w[k] = ln.wk(x, k, method, ...)
+	if (is.hclust) 
+	{	require(cluster)
+		hc = hclust(dist(x), ...)
+	}
+	for (k in 1:max) ln.w[k] = ln.wk(x, if (is.hclust) cutree(hc, k) else method(x, k, ...)$cluster)
     for (b in 1:B)
 	{	r = ref.dist(x, pc)
-		for (k in 1:max) e.ln.ws[b, k] = ln.wk(r, k, method, ...)
+		if (is.hclust) r.hc = hclust(dist(r))
+		for (k in 1:max) e.ln.ws[b, k] = ln.wk(r, if (is.hclust) cutree(hc, k) else method(x, k, ...)$cluster)
 	}
 	e.ln.w = colMeans(e.ln.ws)
 	for (k in 1:max) s[k] = sqrt((1 + 1/B) * sum((e.ln.ws[,k] - mean(e.ln.ws[,k]))^2)/B)
